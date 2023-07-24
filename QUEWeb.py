d7bec853c8080e5f662a3380260532d314760e88
adibahzairul <116578497+adibahzairul@users.noreply.github.com>
2023-07-24 22:55:15 +0800

    Update QUEWeb.py

--- QUEWeb.py
+++ QUEWeb.py
@@ -103,7 +103,7 @@ with st.container():
 if st.button("Submit"):
     
     # Unpickle classifier
-    xgbc = joblib.load('xgbc.joblib')
+    xgbc = joblib.load('xgbc')
     
     # Store inputs into dataframe
     X = pd.DataFrame([[V,H,NumHDonors,NumHeteroatoms,ExactMolWt,K,S,NumSaturatedCarbocycles,M,NumSaturatedRings]], 
